7dbada0396727e38dc720b8b2e5b33d1f7f39d49
Christian Goethert <csgoethert@login2.odo.olcf.ornl.gov>
2025-07-21 16:04:55 -0400

    Added update to math2.sh

--- intro-to-bash/arithmetic/math2.sh
+++ intro-to-bash/arithmetic/math2.sh
@@ -4,7 +4,7 @@ a=10
 b=3
 c=2.3
 
-echo "scale=2; $a / $b" | bc  #Note the use of the | pipe operator AND the referal of variables with $
+echo "scale=2; $a / $b" | bc  #MUST use the referal of variables with $ to refer to their values here. Notice the | pipe operator.
 #3.33
 echo "scale=3; $a / $b" | bc  #More accuracy than previous
 #3.333
@@ -15,4 +15,8 @@ echo "scale=4; s(1.571)" | bc -l  #Sine of ~(pi / 2)
 #1.0000
 echo "scale=4; l(e(2))" | bc -l  #Natural log of e^2
 #1.9999
+
+
+echo "scale=3; ($a / $b) + $c" | bc  #Example of compound equation with floats
+#5.633
 
