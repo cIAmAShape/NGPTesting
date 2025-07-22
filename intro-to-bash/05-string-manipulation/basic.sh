12af3ab3342b7ee37924e23ee6efa9064be30c1b
Christian Goethert <csgoethert@login2.odo.olcf.ornl.gov>
2025-07-22 14:27:39 -0400

    Slight typo fix

--- intro-to-bash/05-string-manipulation/basic.sh
+++ intro-to-bash/05-string-manipulation/basic.sh
@@ -7,7 +7,7 @@ echo ${str1: -6}  #They can even be indexed from the end!
 str2="mSD90sfmjo"
 echo ${#str2}  #Gets the length of the string
 
-str3="I dont like bash. I dont like linux."
+str3="I dont like bash. I dont really like bash."
 echo ${str3/dont/really}  #Local replacement (replaces first instance)
 echo ${str3//dont/really}  #Global replacement (replaces ALL instances)
 
