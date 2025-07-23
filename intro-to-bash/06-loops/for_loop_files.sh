f644fe1985cfa2ac5706e29365e3ae8332ecfcae
Christian Goethert <csgoethert@login2.odo.olcf.ornl.gov>
2025-07-23 10:21:32 -0400

    Updated files with comments

--- intro-to-bash/06-loops/for_loop_files.sh
+++ intro-to-bash/06-loops/for_loop_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in file*; do
+for file in file*; do  #file* finds all files with name 'fileX' followed by whatever characters 'X' and onward.  
 	echo "${file} contents:"
 	cat $file
 	echo ""
