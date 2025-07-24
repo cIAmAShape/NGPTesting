ed90d8493c01a0a7c7b85c9e6a166a6b9c12f48d
Christian Goethert <csgoethert@login2.odo.olcf.ornl.gov>
2025-07-24 08:44:45 -0400

    Fixed challenge 08.03 - Junkfile

--- intro-to-bash/08-practice-challenges/solutions/03-Junkfile.sh
+++ intro-to-bash/08-practice-challenges/solutions/03-Junkfile.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-echo "Line # | Line Length | Line Contents"
-awk '{print NR, length, $0}' noise.txt | sort -k2 -nr | head -1
+f_Size=0
+f_Name=noise0.txt
 
-#Explanation of code:
+cd noise  #Go's into "noise" directory
+for file in *.txt; do  #Iterates through the files
+	c_Size=$(wc -l < $file)  #Sets variable cur_Size to the value of command wc from $file
+	if [[ $c_Size -gt $f_Size ]]; then #Compares for biggest one
+		f_Size=$c_Size
+		f_Name=$file
+	fi	
+done
+
+echo "Line # | Line Length | Line Contents"
+awk '{print NR, length, $0}' $f_Name | sort -k2 -nr | head -1
+
+# Explanation of code:
 #	
 #	awk 'print NR, length, $0'  
 #		prints every line of file noise.txt
@@ -13,14 +25,9 @@ awk '{print NR, length, $0}' noise.txt | sort -k2 -nr | head -1
 #
 #	head -1
 #		prints only the head (the top value, in this case the largest size)
-#
-
-
-
-
+# WARNING: This code will not work without the noise subdirectory to search through.
 
 
 #This is the power of pipes! Don't be afraid to use them!!
 #Bonus not included, though...
-
 
